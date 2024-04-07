cd483512cbba1137c2ead146eea808250dbdc950
elehmandevelopment <elehman.development@gmail.com>
2024-04-06 23:07:30 -0600

    update team

--- app/components/OurTeam/OurTeam.tsx
+++ app/components/OurTeam/OurTeam.tsx
@@ -22,28 +22,28 @@ const people = [
     linkedinUrl: 'https://www.linkedin.com/in/ianbrillembourg/'
   },
   {
-    name: 'Matt Polvi',
-    role: 'Operations',
-    image: matt,
-    linkedinUrl: 'https://www.linkedin.com/in/mattpolvi111/'
-  },
-  {
     name: 'Jack Davanzo',
     role: 'Business Development',
     image: jack,
     linkedinUrl: 'https://www.linkedin.com/in/jack-davanzo/'
   },
   {
-    name: 'Emma Sharma',
-    role: 'Product Lead',
-    image: emma,
-    linkedinUrl: 'https://www.linkedin.com/in/emmasharma/'
-  },
-  {
     name: 'Ethan Lehman',
     role: 'Developer',
     image: ethan,
     linkedinUrl: 'https://www.linkedin.com/in/ethan-lehman-development/'
+  },
+  {
+    name: 'Matt Polvi',
+    role: 'Operations',
+    image: matt,
+    linkedinUrl: 'https://www.linkedin.com/in/mattpolvi111/'
+  },
+  {
+    name: 'Emma Sharma',
+    role: 'Product Lead',
+    image: emma,
+    linkedinUrl: 'https://www.linkedin.com/in/emmasharma/'
   }
 ]
   
@@ -63,8 +63,8 @@ return (
         className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8"
         >
         {people.map((person) => (
-            <li key={person.name} className="rounded-2xl bg-gray-800 px-8 py-10">
-            <Image className="mx-auto h-48 w-48 rounded-full md:h-56 md:w-56" src={person.image} alt="" />
+            <li key={person.name} className="rounded-2xl bg-gray-800 py-8">
+            <Image className="mx-auto h-40 w-40 rounded-full" src={person.image} alt="" />
             <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-white">{person.name}</h3>
             <p className="text-sm leading-6 text-gray-400">{person.role}</p>
             <ul role="list" className="mt-6 flex justify-center gap-x-6">
